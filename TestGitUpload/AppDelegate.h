f6f50981a1826def318caaa27cc8872ab9c6b7ca
Aseem 1 <aseem1@Aseems-Mac-Pro-3.local>
2015-11-19 10:31:12 +0530

    Initial Commit

---
+++ TestGitUpload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGitUpload
+//
+//  Created by Aseem 1 on 19/11/15.
+//  Copyright (c) 2015 codeBrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
